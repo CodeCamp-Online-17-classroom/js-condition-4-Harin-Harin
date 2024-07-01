294f31a85aae126bc827fe24e3a7eb796d5dc549
Harin-Mabao <mixharinmabao@gmail.com>
2024-07-01 20:33:59 +0700

    Update app.js Rev01

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let a = prompt("Guess my name?")
-if (a === 'Harin'||'harin'||'mig'||'Mig') {
+if (a === 'Harin' || a === 'harin' || a === 'mig' || a === 'Mig') {
     alert('Yes');
 } else {
     alert('No');}
